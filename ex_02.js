5c59d3ab4d936987d89ba37ce2fa33cc03b75b17
EM02-AL <eddy.mazi@epitech.digital>
2024-09-04 16:47:02 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,14 +1,14 @@
-function findHousing(housingToFind, housingList) { 
-let index = 0; 
-housingToFind = housingToFind.toLowerCase(); 
+function findHousing(housingToFind, housingList) {
+    let index = 0;
+    housingToFind = housingToFind.toLowerCase();
 
-while (index < housingList.lenght){
-if (housingList[index].toLowerCase() === housingToFind) {
-return [index+1, housingList[index]]; 
- }
- index++;
-}
-return null; 
+	while (index < housingList.length) {
+        if (housingList[index].toLowerCase() === housingToFind) {
+            return [index + 1, housingList[index]];
+        }
+        index++;
+    }
+    return null;
 }
 
-displayResult(findHousing("House",["Flat", "House", "Hut", "House", "Igloo"]))
+displayResult(findHousing("Hut", ["Flat", "House", "Hut", "House", "Igloo"]))
